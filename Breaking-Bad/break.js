8b24dd101c913c47b015baa5e4a7f5984410a7f1
abiclub23 <abiclub@gmail.com>
2015-05-12 14:52:25 -0400

    js redone
    restructured and optimized

--- Breaking-Bad/break.js
+++ Breaking-Bad/break.js
@@ -1,348 +1,240 @@
-// restrucute js code
-// add elements
-// atomic wieght display
-// input validation
-//convert to image
+//optimizing js code
+// add.reomve hidden/show
+ var ww; // for walter white
+ var breakingBad = {
 
-var array_name = [];
-     var single_match_index = [];
-     var lname_single_match_index = [];
-     var lname_single_match_index = [];
-     var  double_match_index = [];
-     var  lname_double_match_index = [];
-      var fname,fname_len,lname,lname_len,double_match_lname,double_match;
-      var fname_ename_double = [], fname_ename_single = [];
-      var lname_ename_double = [], lname_ename_single = [];
+ 	settings : {
+ 		
+ 		mutateInput: '',
+ 		elements: [],
+ 		singleMatch_inputIndex: [],
+		lname_singleMatch_inputIndex: [],
+ 		lname_singleMatch_inputIndex: [],
+ 		doubleMatch_inputIndex: [],
+ 		doubleMatch_elemIndex: [],
+   		singleMatch_elemIndex: [],
+ 		periodic: table
+ 	},
 
+ 	init: function(){
 
-$periodic =[
-    {
-        "name": "Hydrogen",
-        "symbol": "H",
-        "atomicWeight": 1.0079
-    },
-    {
-        
-        "name": "Helium",
-        "symbol": "He",
-        "atomicWeight": 4.0026
-    },
-    {
-        
-        "name": "Lithium",
-        "symbol": "Li",
-        "atomicWeight": 6.941
-    },
-    {
-        
-        "name": "Beryllium",
-        "symbol": "Be",
-        "atomicWeight": 9.0122
-    },
-    {
-        
-        "name": "Boron",
-        "symbol": "B",
-        "atomicWeight": 10.811
-    },
-    {
-        
-        "name": "Carbon",
-        "symbol": "C",
-        "atomicWeight": 12.0107
-    },
-    {
-       
-        "name": "Nitrogen",
-        "symbol": "N",
-        "atomicWeight": 14.0067
-    },
-    {
-       
-        "name": "Oxygen",
-        "symbol": "O",
-        "atomicWeight": 15.9994
-    },
-    {
-       
-        "name": "Fluorine",
-        "symbol": "F",
-        "atomicWeight": 18.9984
-    },
-    {
-        
-        "name": "Neon",
-        "symbol": "Ne",
-        "atomicWeight": 20.1797
-    },
-    {
-        
-        "name": "Sodium",
-        "symbol": "Na",
-        "atomicWeight": 22.990
-    },
-    {
-        
-        "name": "Magnesium",
-        "symbol": "Mg",
-        "atomicWeight": 24.305
-    },
-    {
-       
-        "name": "Aluminum",
-        "symbol": "Al",
-        "atomicWeight": 26.982
-    },
-    {
-       
-        "name": "Silicon",
-        "symbol": "Si",
-        "atomicWeight": 28.085
-    },
-    {
-       
-        "name": "Phosphorus",
-        "symbol": "P",
-        "atomicWeight": 30.974
-    },
-    {
-       
-        "name": "Sulfur",
-        "symbol": "S",
-        "atomicWeight": 32.06
-    },
-    {
-       
-        "name": "Chlorine",
-        "symbol": "Cl",
-        "atomicWeight": 35.45
-    },
-    {
-       
-        "name": "Argon",
-        "symbol": "Ar",
-        "atomicWeight": 39.948
-    }
-]
+ 		ww = this.settings;
+ 		$submitButton= $('#submit');
+ 		$result = $('#result'); // div id to print result
+ 		$first_name = $("#fname"); // first name
+ 		$last_name = $("#lname"); // last name
+ 		$alert = $('.alert-danger.custom');
+ 		$warning = $('.bg-success');
+ 		breakingBad.checkifIE();
+ 		breakingBad.bindUIActions();
+ 		
+ 		
+ 	},
+
+ 	bindUIActions: function() {
+	    
+	    $submitButton.on("click", function() {
+	      
+	      $result.empty();
+	      breakingBad.processInput.call($first_name);
+	      breakingBad.processInput.call($last_name);
+	      $('[data-toggle="tooltip"]').tooltip();
+	    });
+
+	    $('[data-toggle="popover"]').popover(); // bootstrap popover init
 
 
-function generate_name (index , index2 ,  uname , ename) {
-       // body...
-       console.log(uname, "uname")
-       console.log(index, "index");
-       console.log(index2,"index2");
-       console.log(ename,"ename");
-      // $('#result').empty();
-       var uname_len = uname.length;
-       //uname = uname[0].toUpperCase();
-       var new_html = "";
-       var name = "";
-       var nochem = true;
-       
-       if(ename.length > 0)
-       {
-        name = $periodic[ename[0]].name;
-       }
-        
-      // new_html += '<span class="atomic">12</span>';
-       for( var j=0; j< uname_len;j++)
-         {
-               if( j == index[0])
-               { 
-                    nochem = false;
-                     new_html += '<span class="font" data-toggle="tooltip" data-original-title= "YO ,its '+name+' Bitch " >'+uname[j].toUpperCase()+'</span>';
-               }
-               else if ( j == index2[0])
-               {
-                     nochem = false;
-                     new_html += '<span class="font" data-toggle="tooltip" data-original-title="YO ,its '+name+' Bitch ">'+uname[j].toUpperCase()+''+uname[j+1]+'</span>';
-               }
-               else
-               {
-                    if(index2)
-                    {
-                        if( j == (index2[0]+1))
-                        {
-                            console.log("im being skipped");
-                        }
-                         else
-                        {
-                            new_html += '<span >'+uname[j]+'</span>';
-                        }
+		$.each(ww.periodic, function(i, symbol) { ww.elements.push(ww.periodic[i].symbol.toLowerCase()); }); // fetching periodic table data
+		
+		$(document).keydown(function(e) { if (e.keyCode == 13) { $submitButton.click(); } }); //checking enter key press
+  	},
 
-                     
-                    }
-                    else
-                    {
-                        new_html += '<span >'+uname[j]+'</span>';
-                    }
-               } 
-                   
-                
-
-         }
-        console.log(nochem ,"nochem") ;
-
-        /*if(nochem)
-        {
-            $('.bg-success').removeClass('hidden').addClass('show');
-            return false;
+  	/* function to check IE - taken from css tricks */
+  	checkifIE : function() {
+  		var isMSIE = /*@cc_on!@*/1;
+    	if (!isMSIE) {
+            // do IE-specific things
+            $("#ie-msg").removeClass("hidden");
         }
-        else
-        {
-            $('.bg-success').addClass('hidden').removesClass('show');
-           
-        }*/
+  	},
 
-        $('#result').append('<span class="wrap"> '+new_html+' </span>');
-       
-   }
+  	resetAll: function(){
 
-
-$(document).ready(function() {
-
-    $.each($periodic, function(i, symbol) {
-            
-         array_name.push($periodic[i].symbol.toLowerCase());
-           
-    });
-
-   /* $(window).resize(function(){    
-        if ($(window).width() <= 480){  
+  		ww.singleMatch_inputIndex.length = 0;
+  		ww.doubleMatch_inputIndex.length = 0;
+        ww.singleMatch_elemIndex.length = 0;
+        ww.doubleMatch_elemIndex.length = 0;
         
-                $(".input-group").removeClass('input-group-lg');
-        }
-
-  });*/
-     
-   $('#submit').click(function(){
-
-    console.log ("click")
-
-       single_match_index.length = 0;
-       double_match_index.length = 0;
-       fname_ename_single.length = 0;
-       fname_ename_double.length = 0;
-       lname_ename_single.length = 0;
-       lname_ename_double.length = 0;
-       lname_single_match_index.length = 0;
-       lname_double_match_index.length = 0;
-
-        $('#result').empty();
-        fname = $("#fname").val().toLowerCase();
-        fname_len = fname.length;
-        console.log(fname)
-        lname = $("#lname").val().toLowerCase();
-        lname_len = lname.length;
         
 
+  	},
 
-        var regexp1=new RegExp("[^a-z]");
-        if(regexp1.test(fname))
-        {
+  	processInput: function() {
+  		
+  		breakingBad.resetAll(); // resetting all arrays
+  		var $curr = $(this);
+  		var userInputName, userInputName_len;
+  		var elem_len = ww.elements.length;
+  		var regexp1 = new RegExp("[^a-z]");
+  		if($curr.val().length == 0) { return;} //do nothing when input is empty
+  		
+  		userInputName = $curr.val().toLowerCase();
+        userInputName = userInputName.replace(/\s+/g, '');
+        userInputName_len = userInputName.length;
+        
+        if (regexp1.test(userInputName) ) {
             //alert("Only alphabets from a-z are allowed");
-            $('.alert-danger').removeClass('hidden').addClass('show');
+            $alert.removeClass('hidden').addClass('show');
+            $warning.addClass('hidden').removeClass('show');
             return false;
+        } else {
+            $alert.addClass('hidden').removeClass('show');
         }
-        else
-            { $('.alert-danger').addClass('hidden').removeClass('show'); }
-         console.log(array_name);
-         // compare first name
-         for( var j=0; j< fname_len;j++)
-         {
-            for (var k = 0 ; k < array_name.length ; k++)
-            {
+        outerloop: 
+        for (var j = 0; j < userInputName_len; j++) {
+        	
+            for (var k = 0; k < elem_len; k++) {
                 // Single Char Compare
-                if ( fname[j] == array_name[k])
-                {
-                   // alert (array_name[k]);
-                    single_match_index.push(j);
-                     fname_ename_single.push(k);
-                    //console.log(fname_element_name, "fname_element_name")
-                    console.log(single_match_index, "single_match_indexs")
+               if(ww.doubleMatch_inputIndex == 0 && ww.singleMatch_inputIndex.length == 0){ // if a match is found dont search anymore
+	                if (userInputName[j] == ww.elements[k]) {
+	                    
+	                    ww.singleMatch_inputIndex.push(j);
+	                    ww.singleMatch_elemIndex.push(k); 
 
+	                    
+	                }
                 }
                
+
                 // Double Char Compare
-                if(typeof(fname[j+1]) !== "undefined" )
-                {
-                    double_match = fname[j]+fname[j+1];
-                    //console.log("double_match" , double_match)
-                    if(double_match == array_name[k])
-                    {
-                        fname = fname.replace(fname[j],double_match);
-                        fname = fname.slice(0, j+1) + fname.slice(j+2, fname.length);
-                        fname_ename_double.push(k);
-                        double_match_index.push(j);
-                        console.log("match double" ,fname )
+                if (typeof(userInputName[j + 1]) !== "undefined") {
+                    ww.mutateInput = userInputName[j] + userInputName[j + 1];
+                   
+                    if (ww.mutateInput == ww.elements[k]) {
+                        
+                        ww.doubleMatch_inputIndex.push(j);
+                        ww.doubleMatch_elemIndex.push(k);
+                        
+                        break outerloop; // breaking loop when I find 1st match.
                     }
 
                 }
-                
+
             }
-         }
-         
-        if(double_match_index.length)
-         {
-            generate_name("" ,double_match_index ,fname,fname_ename_double);
-            console.log("generate double_match - fname_ename_double" ,fname_ename_double )
-         }
-         else
-         {
-            generate_name(single_match_index , "" ,fname,fname_ename_single);
-            console.log("generate single_match - fname_ename_single",fname_ename_single)
-         }
-
-         for( var j=0; j< lname_len;j++)
-         {
-            for (var k = 0 ; k < array_name.length ; k++)
-            {
-                if ( lname[j] == array_name[k])
-                {
-                   // alert (array_name[k]);
-                    lname_single_match_index.push(j);
-                    lname_ename_single.push(k);
-                    console.log(lname_single_match_index, "single_match_index_Last Name")
-
-                }
-
-                if(typeof(lname[j+1]) !== "undefined" )
-                {
-                    double_match_lname = lname[j]+lname[j+1];
-                    //console.log("double_match" , double_match)
-                    if(double_match_lname == array_name[k])
-                    {
-                        //lname = lname.replace(lname[j],double_match_lname);
-                       // lname = lname.slice(0, j+1) + lname.slice(j+2, lname.length);
-                        lname_double_match_index.push(j);
-                        lname_ename_double.push(k);
-                        console.log("match double" ,lname )
-                    }
-
-                }
-            }
-         }
-        //generate name
-        if(lname_double_match_index.length)
-         {
-           generate_name("" ,lname_double_match_index ,lname, lname_ename_double);
-            console.log("generate double_match")
-         }
-         else
-         {
-            generate_name(lname_single_match_index , "" ,lname, lname_ename_single);
-            console.log("generate single_match")
-         }
+        }// end for
         
-         $(".font").tooltip({ placement : 'top' });
-        
-   });// end submit
+        if (ww.doubleMatch_elemIndex) { // if mutate text has a match
+            ww.single_match_index = "";
+            
+            breakingBad.generateName(userInputName,ww.single_match_index, ww.doubleMatch_inputIndex,  ww.doubleMatch_elemIndex);
+            
+           
+        } else { //only single char match
+        	ww.doubleMatch_inputIndex = "";
+        	
+            breakingBad.generateName(userInputName, ww.single_match_index, ww.doubleMatch_inputIndex ,  ww.singleMatch_elemIndex);
+            
+            
+        }
+
+  	},
+
+  	generateName: function(userName,singleIndex,doubleIndex,elementNameIndex){
+
+  		var uname = userName;
+  		
+  		var uname_len = userName.length;
+  		var ename = elementNameIndex;
+  		
+	    var ename_len = ename.length;
+	    var resultHtml = "";
+	    var name, atomic_num, molar;
+	    var nochem = true;
+	    
+
+	    if (ename.length > 0) { // using ename[0] to display the 1st match , when there are multiple matches.
+	        name = ww.periodic[ename[0]].name; console.log ( "name" , name);
+	        atomic_num = ww.periodic[ename[0]].number; console.log ( "atomic_num" , atomic_num);
+	        molar = ww.periodic[ename[0]].molar.toFixed(2); console.log ( "molar" , molar);
+    	}
+    	for (var j = 0; j < uname_len; j++) {
+	        if (j == singleIndex[0]) {
+	            nochem = false;
+	            resultHtml += '<span class="font" data-toggle="tooltip" data-placement="top" data-original-title= "YO ,its ' + name + ' " >' + uname[j].toUpperCase() + '</span>';
+
+	        } else if (j == doubleIndex[0]) {
+	            nochem = false;
+	            resultHtml += '<span class="font second" data-toggle="tooltip" data-placement="top" data-original-title="YO ,its ' + name + '  ">' + uname[j].toUpperCase() + '' + uname[j + 1] + '</span>';
+	            console
+	        } else {
+	            if (doubleIndex) {
+	                if (j == (doubleIndex[0] + 1)) {
+	                    //console.log("im being skipped");
+	                } else {
+	                    resultHtml += '<span >' + uname[j] + '</span>';
+	                }
+
+
+	            } else {
+	                resultHtml += '<span >' + uname[j] + '</span>';
+	            }
+	        }
+    	}
+
+    	if (ename.length > 0) {
+	        resultHtml += '<span class="atomic">' + atomic_num + '</span> <span class="molar">' + molar + '</span>';
+	    }
+	    
+
+	   
+
+	    if (nochem && !($('.wrap').length)) {
+	        $warning.removeClass('hidden').addClass('show');
+	        $alert.addClass('hidden').removeClass('show');
+	        return false;
+	    } else {
+	        $warning.addClass('hidden').removeClass('show');
+
+	    }
+
+	     $result.append('<span class="wrap"> ' + resultHtml + ' </span>');
+
+	     // calculating position for atomic number placement
+	    var leftPos1 = $(".font").position().left;
+	    var topPos1 = $(".font:last").height();
+	    var leftPos2 = $(".font:last").position().left;
+	    var topPos2 = $(".font:last").position().top + $(".font:last").height() - 4;
+
+	    leftPos1 = Math.round(leftPos1);
+	    leftPos2 = Math.round(leftPos2);
+	    $(".atomic").css({
+	        'left': leftPos1,
+	        'top': topPos1
+	    });
+
+	    $(".atomic:last").css({
+	        'left': leftPos2,
+	        'top': topPos2
+	    });
+	    $(".molar").css('left', leftPos1);
+	    $(".molar:last").css('left', leftPos2);
+
+	    if ($('.wrap:last .font').length == 0) {
+	        $('.wrap:last').empty();
+	    }
+
+
+
+  	}// end GenerateName
+
+
+ };
+
+ $(document).ready(function(){
+ 	breakingBad.init();
+   
+ });
 
   
+ 
 
-
-   
-   
-
-   
-});
